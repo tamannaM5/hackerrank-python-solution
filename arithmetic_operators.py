9689d50e0218d92bc1fb6b4abb2701c010f46068
Tamanna Mukherjee <128417568+tamannaM5@users.noreply.github.com>
2026-07-16 00:33:05 +0530

    Solve Arithmetic Operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+#Problem: Arithmetic Operators
+#Platform: HackerRank
+#Difficulty: Easy
+
+#Concepts:- Arithmetic Operators - Input/Output
+
+#Time Complexity: O(1)
+#Space Complexity: O(1)
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
